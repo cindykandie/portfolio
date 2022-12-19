37c64d6faf876a029c4a6c2f622a9c27be963fb8
Joshua Lyngdoh <joshualyngdoh52@gmail.com>
2022-12-19 21:22:51 +0530

    Changes to JS file

--- js/navbar.js
+++ js/navbar.js
@@ -1,18 +1,21 @@
 let img = document.getElementById("navbar")
+let menu = document.querySelector(".desktop-nav")
+
 img.addEventListener('click', toggleNav)
 
 function toggleNav() {
-
+  function toggleMenu() {
+    menu.style.left = 0;
+    menu.style.right = 0;
+  }
     if (img.src.match("assets/menu.png") ) {
       img.src = "assets/closing-icon.svg";
+      toggleMenu() 
     } 
     else{
       img.src = "assets/menu.png";
     }
 }
-
-
-
 
 
 
