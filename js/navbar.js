0e4c6399545b900eb3b504371cd8476ecaad4f14
cindykandie <cindyjk6@gmail.com>
2022-12-19 17:49:47 +0300

    add x icon on navbar

---
+++ js/navbar.js
@@ -0,0 +1,18 @@
+let img = document.getElementById("navbar")
+img.addEventListener('click', toggleNav)
+
+function toggleNav() {
+
+    if (img.src.match("assets/menu.png") ) {
+      img.src = "assets/closing-icon.svg";
+    } 
+    else{
+      img.src = "assets/closing-icon.svg";
+
+    }
+}
+
+
+  // Step1: Toggle menubar with the closing icon
+  // Step2: Style the moble menu
+  // Step3: Combine the events
--- navbar.js
+++
@@ -1,9 +0,0 @@
-let img = document.querySelector("navbar")
-function toggleNav(e) {
-  // toggle menubar with the closing icon
-  // make visible the navigation
-  if (img.src = "assets/menu.png" ) {
-    img.src = "assets/closing-icon.svg";
-  } 
-  console.log();
-}
