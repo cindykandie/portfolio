617b895cac150fc47b55e7490f305c1f1a970c4f
Joshua Lyngdoh <joshualyngdoh52@gmail.com>
2022-12-19 18:45:00 +0530

    Added eslint file

--- navbar.js
+++ navbar.js
@@ -1,4 +1,9 @@
-function toggleNav(){
-    //toggle menubar with the closing icon
-    // make visible the navigation
+let img = document.querySelector("navbar")
+function toggleNav(e) {
+  // toggle menubar with the closing icon
+  // make visible the navigation
+  if (img.src = "assets/menu.png" ) {
+    img.src = "assets/closing-icon.svg";
+  } 
+  console.log();
 }
