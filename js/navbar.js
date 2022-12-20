87f1572e4bfc5af70ffa40f4f254cb294ce60ea2
cindykandie <cindyjk6@gmail.com>
2022-12-20 11:27:18 +0300

    fix linter errors

--- js/navbar.js
+++ js/navbar.js
@@ -1,7 +1,6 @@
-let img = document.getElementById('navbar');
-let menu = document.querySelector('.desktop-nav');
-let specs = document.querySelectorAll(".specs")
-
+const img = document.getElementById('navbar');
+const menu = document.querySelector('.desktop-nav');
+const specs = document.querySelectorAll('.specs');
 
 function toggleNav() {
   if (img.src.match('assets/menu.png')) {
@@ -13,13 +12,13 @@ function toggleNav() {
   }
 }
 function closeMenu() {
-  menu.classList.toggle("active")
-  img.src = "assets/menu.png";
+  menu.classList.toggle('active');
+  img.src = 'assets/menu.png';
 }
 img.addEventListener('click', toggleNav);
-  specs.forEach((element) => {
-  element.addEventListener('click', closeMenu)
-})
+specs.forEach((element) => {
+  element.addEventListener('click', closeMenu);
+});
 
 // Step1: Toggle menubar with the closing icon
 // Step2: Style the moble menu
