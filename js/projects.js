02cd899abcec9927d511c70d468af999e1c8d68a
cindykandie <cindyjk6@gmail.com>
2022-12-23 11:25:26 +0300

    final changes in the popup js and css

--- js/projects.js
+++ js/projects.js
@@ -54,7 +54,7 @@ const projects = [
     image: '<img src="assets/multi.png" alt="Snapshoot Portfolio image"/>',
     description1: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
     description2: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
-    tools: ['html, css, javascript'],
+    tools: ['html', 'css', 'javascript'],
     tools2: ['html', 'css', 'javascript', 'ruby','github','bootstrap'],
     seeProjectbtn: 'See Project',
     liveLink: '#',
